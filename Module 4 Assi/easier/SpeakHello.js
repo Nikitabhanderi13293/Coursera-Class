e5bbf5a542fcd407cf92aed1c5c3c0517a04ea95
Nikitabhanderi13293 <60956110+Nikitabhanderi13293@users.noreply.github.com>
2020-02-22 18:49:50 +0530

    Update SpeakHello.js

--- Module 4 Assi/easier/SpeakHello.js
+++ Module 4 Assi/easier/SpeakHello.js
@@ -17,7 +17,7 @@ var speakWord = "Hello";
 (function (window) {
   var helloSpeaker = {};
   helloSpeaker.speak = function speak(name){
-  console.log(speakWord + name);
+  console.log(speakWord + " " + name);
   }
  window.helloSpeaker= helloSpeaker;
 })(window);
