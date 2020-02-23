89dc1d26c3d943bc1ef9bd00002cc13b0bf3a7f0
Nikitabhanderi13293 <60956110+Nikitabhanderi13293@users.noreply.github.com>
2020-02-23 10:37:14 +0530

    Update SpeakGoodBye.js

--- Module 4 Assi/easier/SpeakGoodBye.js
+++ Module 4 Assi/easier/SpeakGoodBye.js
@@ -18,8 +18,8 @@ var speakWord = "Good Bye";
 // See Lecture 52, part 2
 (function (window) {
   var byeSpeaker={};
-  byeSpeaker.name = function speak(name) {
-  console.log(speakWord + " " + byeSpeaker.name);
+  byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
   }
   window.byeSpeaker= byeSpeaker;
 })(window);
